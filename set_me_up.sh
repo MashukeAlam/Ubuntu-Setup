3bb233231cf0ca937064928f7ee1f1460c8e6b0f
Mashuke Alam Jim <jimmashuke@gmail.com>
2024-06-14 16:23:59 +0600

    for zsh done

--- set_me_up.sh
+++ set_me_up.sh
@@ -1,66 +1,67 @@
-echo "Welcome! Let's set this machine up"
-echo "Remember that this process will require sudo permission."
+#!/bin/bash
 
-echo "What is your Full name?"
+# Function to print colored messages
+print_message() {
+  local message=$1
+  echo -e "\e[1;34m${message}\e[0m"
+}
+
+print_message "Welcome! Let's set this machine up"
+print_message "Remember that this process will require sudo permission."
+
+print_message "What is your Full name?"
 read git_config_user_name
 
-echo "What is your Github username?"
+print_message "What is your Github username?"
 read username
 
-echo "What email do you want to use to log into Github?"
+print_message "What email do you want to use to log into Github?"
 read git_config_user_email
 
-echo "Updating system"
+print_message "Updating system"
 cd ~ && sudo apt update
 
-echo "Installing figlet"
-sudo apt install figlet -y
-
-figlet "Installing lolcat"
-sudo snap install lolcat
-
-figlet 'Installing curl'  | lolcat
+print_message "Installing curl"
 sudo apt-get install curl -y
 
-figlet "Installing EXA" | lolcat
+print_message "Installing EXA"
 sudo apt-get install exa -y
 
-figlet 'Installing neofetch'  | lolcat
+print_message "Installing neofetch"
 sudo apt-get install neofetch -y
 
-figlet 'Installing bat'  | lolcat
+print_message "Installing bat"
 sudo apt-get install bat -y
 
-
-figlet "Installing copyq" | lolcat
+print_message "Installing copyq"
 sudo add-apt-repository ppa:hluk/copyq -y
 sudo apt update
 sudo apt install copyq -y
 
-figlet 'Installing latest git'  | lolcat
+print_message "Installing latest git"
 sudo add-apt-repository ppa:git-core/ppa -y
 sudo apt-get update && sudo apt-get install git -y
 
-figlet 'Installing python3-pip' | lolcat
+print_message "Installing python3-pip"
 sudo apt-get install python3-pip -y
 
-figlet "Setting up your git global user name and email" | lolcat
+print_message "Setting up your git global user name and email"
 git config --global user.name "$git_config_user_name"
 git config --global user.email $git_config_user_email
 
-figlet 'Installing FiraCode' | lolcat
+print_message "Installing FiraCode"
 sudo apt-get install fonts-firacode -y
 
-figlet 'Installing SF Pro font' | lolcat
+print_message "Installing SF Pro font"
 wget https://www.cufonfonts.com/get/font/download/d164d0130677c7a1d5bf59c5177e95cc
 
-figlet "Installing Monaco font" | lolcat
+print_message "Installing Monaco font"
 wget 'https://github.com/todylu/monaco.ttf/blob/master/monaco.ttf'
 
-figlet "Installing Starship" | lolcat
+print_message "Installing Starship"
 curl -sS https://starship.rs/install.sh | sh
 
-figlet "Installing Ble.sh" | lolcat
+print_message "Installing Ble.sh"
 cd ~
 git clone --recursive https://github.com/akinomyoga/ble.sh.git
 cd ble.sh
@@ -68,29 +69,36 @@ sudo apt-get install gawk -y
 make install
 cd ~
 
-figlet "Installing macOS theme for all accent color..." | lolcat
+print_message "Installing macOS theme for all accent color..."
 git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git --depth=1
 cd ./WhiteSur-gtk-theme && ./install.sh -t all
 cd ~
 
-figlet "Installing macOS Icon theme" | lolcat
+print_message "Installing macOS Icon theme"
 git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git
 cd WhiteSur-icon-theme && ./install.sh -a
 cd ~
 
-figlet "Installing macOS wallpaper" | lolcat
+print_message "Installing macOS wallpaper"
 git clone https://github.com/vinceliuice/WhiteSur-wallpapers.git
 cd WhiteSur-wallpapers && sudo ./install-gnome-backgrounds.sh
 cd ~
 
-figlet "Installing Gnome tweaks" | lolcat
+print_message "Installing Gnome tweaks"
 sudo apt install gnome-tweak-tool -y
 
-figlet "Doing some customization in gnomes..." | lolcat
+print_message "Installing figlet"
+sudo apt install figlet -y
+
+print_message "Installing fortune"
+sudo apt install fortune -y
+
+
+print_message "Doing some customization in gnomes..."
 gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
 gsettings set org.gnome.desktop.interface gtk-theme 'WhiteSur-Light-solid-green'
 
-figlet 'Installing NVM'  | lolcat
+print_message "Installing NVM"
 sh -c "$(curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh | bash)"
 
 export NVM_DIR="$HOME/.nvm" && (
@@ -102,121 +110,173 @@ git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-figlet 'Installing NodeJS LTS' | lolcat
+print_message "Installing NodeJS LTS"
 nvm --version
 nvm install --lts
 nvm current
 
-figlet "Installing yarn" | lolcat
+print_message "Installing yarn"
 npm i -g yarn
 
-figlet "Installing RBENV" | lolcat
+print_message "Installing RBENV"
 sudo apt install libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev -y
 curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ~/.bashrc
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+source ~/.zshrc
 type rbenv
 
-figlet "Installing Ruby 2.7.7" | lolcat
+print_message "Installing Ruby 2.7.7"
 rbenv install 2.7.7
 rbenv global 2.7.7
 ruby -v
 
-figlet "Installing bundler" | lolcat
+print_message "Installing bundler"
 echo "gem: --no-document" > ~/.gemrc
 gem install bundler
 gem env home
 
-figlet "Installing rails" | lolcat
-gem install rails -v 6.0.1
+print_message "Installing rails"
+gem install rails
 rails -v
 
-figlet "Installing VS Code" | lolcat
+print_message "Installing VS Code"
 sudo snap install code --classic
 
 cd ~
 
-figlet 'Installing MPV' | lolcat
+print_message "Installing MPV"
 sudo apt install mpv -y
 echo UP add volume +2 >> ~/.config/mpv/input.conf
 echo DOWN add volume -2 >> ~/.config/mpv/input.conf
 
-figlet 'Installing OBS Studio' | lolcat
+print_message "Installing OBS Studio"
 sudo apt-get install ffmpeg -y && sudo snap install obs-studio
 
-figlet 'Enabling KVM for Android Studio' | lolcat
+print_message "Enabling KVM for Android Studio"
 sudo apt-get install qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager -y
 sudo adduser $USER libvirt
 sudo adduser $USER libvirt-qemu
 
-figlet 'Installing dbeaver' | lolcat
+print_message "Installing dbeaver"
 wget -c https://dbeaver.io/files/6.0.0/dbeaver-ce_6.0.0_amd64.deb
 sudo dpkg -i -y dbeaver-ce_6.0.0_amd64.deb
 sudo apt-get install -f
 
-figlet 'Installing fzf' | lolcat
+print_message "Installing fzf"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 
-figlet 'Installing Peek'  | lolcat
+print_message "Installing Peek"
 sudo add-apt-repository ppa:peek-developers/stable -y
 sudo apt-get update && sudo apt-get install peek -y
 
-figlet "Installing Google Chrome" | lolcat
+print_message "Installing Google Chrome"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-figlet "Setting up Bash aliases..." | lolcat
-echo 'alias gs="git status"' >> ~/.bashrc
-echo 'alias ga="git add ."' >> ~/.bashrc
-echo 'alias gcm="git commit -m "' >> ~/.bashrc
-echo 'alias commit="git commit -m "' >> ~/.bashrc
-echo 'alias checkout="git checkout "' >> ~/.bashrc
-echo 'alias gb="git branch"' >> ~/.bashrc
-echo 'alias gl="git log"' >> ~/.bashrc
-echo 'alias gp="git push"' >> ~/.bashrc
-echo 'alias h="cd ~"' >> ~/.bashrc
-echo 'alias d="cd ~/Downloads"' >> ~/.bashrc
-echo 'alias p="cd ~/projects"' >> ~/.bashrc
-echo 'alias c="cd ~/.config"' >> ~/.bashrc
-echo 'alias bashrc="nano ~/.bashrc "' >> ~/.bashrc
-echo 'alias iii="sudo apt install -y"' >> ~/.bashrc
-echo 'alias cl="clear"' >> ~/.bashrc
-echo 'alias gacm="ga && gcm"' >> ~/.bashrc
-echo 'alias gg="git status"' >> ~/.bashrc
-echo 'alias gcd="git checkout development"' >> ~/.bashrc
-echo 'alias gpl="git pull"' >> ~/.bashrc
-echo 'alias gps="git push"' >> ~/.bashrc
-echo 'alias c="code"' >> ~/.bashrc
-echo 'alias gdev="git checkout development"' >> ~/.bashrc
-echo 'alias gmain="git checkout main"' >> ~/.bashrc
-echo 'alias kat="batcat"' >> ~/.bashrc
-echo alias ls=\"exa --icons -F -H --group-directories-first\" >> ~/.bashrc##
+print_message "Installing Zsh and Oh-My-Zsh"
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh)
 
+print_message "Installing Zsh Plugins and Themes"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions $ZSH_CUSTOM/plugins/zsh-completions
+sed -i 's/^plugins=(git)/plugins=(git zsh-syntax-highlighting zsh-autosuggestions zsh-completions)/' ~/.zshrc
 
-figlet "Setting up Starship" | lolcat
-echo eval \"\$\(starship init bash\)\" >> ~/.bashrc
+print_message "Configuring Powerlevel10k Theme"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+sed -i 's/^ZSH_THEME=".*"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+
+print_message "Setting up Zsh Aliases..."
+
+print_message "Setting up Zsh Aliases..."
+cat << 'EOF' >> ~/.zshrc
+# File and Directory Management
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias l="ls -lah"
+alias ll="ls -l"
+alias la="ls -A"
+alias mkdir="mkdir -pv"
+alias cp="cp -i"          # Confirm before overwriting
+alias mv="mv -i"          # Confirm before overwriting
+alias rm="rm -i"          # Confirm before deleting
+
+# Git Shortcuts
+alias gco="git checkout"
+alias gcb="git checkout -b"
+alias gm="git merge"
+alias gr="git remote -v"
+alias gst="git status"
+alias gd="git diff"
+alias gds="git diff --staged"
+alias glg="git log --oneline --graph --decorate"
+alias gup="git pull --rebase"
+alias gpr="git pull --rebase && git push"
+alias gclean="git clean -fd"
+
+# System and Utilities
+alias update="sudo apt update && sudo apt upgrade -y"
+alias cleanup="sudo apt autoremove -y && sudo apt autoclean -y"
+alias reboot="sudo reboot"
+alias shutdown="sudo shutdown now"
+alias df="df -h"
+alias free="free -m"
+
+# Networking
+alias ipinfo="curl ipinfo.io/ip"
+alias myip="ip addr show | grep 'inet ' | grep -v '127.0.0.1'"
+
+# Docker
+alias dps="docker ps"
+alias dpsa="docker ps -a"
+alias dbuild="docker build -t"
+alias drun="docker run -it --rm"
+alias dstop="docker stop"
+alias drm="docker rm"
+alias dimages="docker images"
+
+# Kubernetes (kubectl)
+alias k="kubectl"
+alias kx="kubectl exec -it"
+alias kg="kubectl get"
+alias kd="kubectl describe"
+alias kl="kubectl logs"
+alias kctx="kubectl config use-context"
+alias kns="kubectl config set-context --current --namespace"
+
+# Python and Virtual Environments
+alias venv="python3 -m venv"
+alias act="source venv/bin/activate"
+alias deact="deactivate"
+
+# Custom Commands
+alias weather="curl wttr.in"
+alias todo="nano ~/todo.txt"
+alias timestamp="date '+%Y-%m-%d %H:%M:%S'"
+alias stopwatch="echo 'Press Ctrl+C to stop'; date +%s | while read start; do date +%s | awk '{print strftime(\"%H:%M:%S\", $1-'$start', 1)}'; sleep 1; done"
+
+# Zsh Reload
+alias reload="source ~/.zshrc"
+
+# Aliases for common typos
+alias mroe="more"
+alias kk="kubectl"
+alias htop="htop -C"
+EOF
+
+print_message "Setting up Starship"
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
 starship preset nerd-font-symbols -o ~/.config/starship.toml
 
-echo "[[ \${BLE_VERSION-} ]] && ble-attach" >> ~/.bashrc
+print_message "Source new zshrc..."
+source ~/.zshrc
 
-figlet "Source new bashrc..." | lolcat
-source ~/.bashrc
-
-figlet 'Updating and Cleaning Unnecessary Packages' | lolcat
+print_message "Updating and Cleaning Unnecessary Packages"
 sudo -- sh -c 'apt-get update; apt-get upgrade -y; apt-get full-upgrade -y; apt-get autoremove -y; apt-get autoclean -y'
 
-figlet "Alhamdulillah Done!" | lolcat
-
-echo -e '\033[1mPlease add this line at the *TOP* of .bashrc: [[ $- == *i* ]] && source ~/.local/share/blesh/ble.sh --noattach\033[0m'
-# echo -e '\033[1mPlease add this line at the end of .bashrc: eval "$(starship init bash)"\033[0m'
-
-
-
-
-
-
-
-
-
+print_message "Alhamdulillah Done!"
